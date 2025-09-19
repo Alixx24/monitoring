c7d4e503eed32f4bfeb295c03c947566cd556200
Ali Mohammadi <alimohammadi123450@gmail.com>
2025-09-19 12:23:32 -0700

    updated template

--- resources/views/customer/dashboard/index.blade.php
+++ resources/views/customer/dashboard/index.blade.php
@@ -3,46 +3,45 @@
 
 @section('content')
     <a class="btn btn-success ms-5" type="submit" data-bs-toggle="modal" data-bs-target="#createReqModal">create request</a>
+    <a class="btn btn-warning ms-5" type="submit" data-bs-toggle="modal" data-bs-target="#createReqModal">+ unlimited</a>
 
-    <section class="hero-section m-5">
+  <section class="hero-section m-md-5 m-3">
 
+  <div class="bg-light mb-3 p-2">
+    <h5>
+      Email: {{ $user->email }} 
+      <span class="float-end">Wallet: 0</span>
+    </h5>
+  </div>
 
+  <div class="table-responsive">
+    <table class="table">
+      <thead class="thead-light">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Duration</th>
+          <th scope="col">Url</th>
+          <th scope="col">Status</th>
+        </tr>
+      </thead>
 
-        <div class="bg-light mb-5">
-            <h5 class=" p-1">Email: {{ $user->email }} <span class="float-end">Wallet: 0</span></h5>
+      <tbody>
+        @foreach ($fetchRequest as $item)
+          <tr>
+            <th scope="row">1</th>
+            <td>{{ $item->name }}</td>
+            <td>{{ $item->duration_id }} Min</td>
+            <td>{{ $item->url }}</td>
+            <td>{{ $item->status == 1 ? 'Active' : 'Deactive' }}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 
-        </div>
+</section>
 
-        <table class="table">
-            <thead class="thead-light">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Duaration</th>
-                    <th scope="col">Url</th>
-                    <th scope="col">Status</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                @foreach ($fetchRequest as $item)
-                    <tr>
-                        <th scope="row">1</th>
-
-                        <td>{{ $item->name }}</td>
-                        <td>{{ $item->duration_id }} Min</td>
-                        <td>{{ $item->url }}</td>
-                        <td>{{ $item->status == 1 ? 'Active' : 'Deactive' }}</td>
-
-
-
-                    </tr>
-                @endforeach
-
-
-            </tbody>
-        </table>
-    </section>
 
     <x-create-request-modal />
 @endsection
